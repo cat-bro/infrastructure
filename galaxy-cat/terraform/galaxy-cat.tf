8081c1ab119cff15dc0d9cd74c209445410ae8ac
cat-bro <cathjb@gmail.com>
2021-04-27 14:40:24 +1000

    changes to terraform

--- galaxy-cat/terraform/galaxy-cat.tf
+++ galaxy-cat/terraform/galaxy-cat.tf
@@ -1,18 +1,23 @@
 # application server / web server
 resource "openstack_compute_instance_v2" "galaxy-cat" {
-
+ availability_zone = "melbourne-qh2-uom"
+ name = "galaxy-cat"
+ flavor_name = "uom.general.2c8g"
+ key_pair = "nectartest"
+ image_name = "NeCTAR Ubuntu 20.04 LTS (Focal) amd64"
+ security_groups = ["SSH", "Web-Services"]
 }
 
 ## Volume for application/web server
-#resource "openstack_blockstorage_volume_v2" "galaxy-cat-volume" {
-#  availability_zone = "melbourne-qh2"
-#  name        = "galaxy-cat-volume"
-#  description = "galaxy-cat volume"
-#  size        = 100
-#}
+resource "openstack_blockstorage_volume_v2" "galaxy-cat-volume" {
+ availability_zone = "melbourne-qh2-uom"
+ name        = "galaxy-cat-volume"
+ description = "galaxy-cat volume"
+ size        = 80
+}
 
 ## Attachment between application/web server and volume
-#resource "openstack_compute_volume_attach_v2" "attach-dev-volume-to-dev" {
-#  instance_id = "${openstack_compute_instance_v2.galaxy-cat.id}"
-#  volume_id   = "${openstack_blockstorage_volume_v2.galaxy-cat-volume.id}"
-#}
+resource "openstack_compute_volume_attach_v2" "attach-galaxy-cat-volume-to-galaxy-cat" {
+ instance_id = "${openstack_compute_instance_v2.galaxy-cat.id}"
+ volume_id   = "${openstack_blockstorage_volume_v2.galaxy-cat-volume.id}"
+}
